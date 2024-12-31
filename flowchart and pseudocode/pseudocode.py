01fb751ebf678579801d48b0ecf66e719587db2b
devcodes001 <devnarayanpunthala3@gmail.com>
2024-12-31 20:59:10 +0530

    dsa

---
+++ flowchart and pseudocode/pseudocode.py
@@ -0,0 +1,15 @@
+'''
+pseudo code is a simple way of describing an algorithm 
+in plain language or a mix of code like and natural language
+instructions
+
+pseudo code for sum of 2 numbers
+------------------------------->
+Begin
+    Input First Number
+    Input second number
+    sum = first number + second number
+    print sum
+end
+
+'''
