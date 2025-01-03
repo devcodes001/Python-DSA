7240a5ae3ba9beabdc692f3676fd3e3c204e7ac1
devcodes001 <devnarayanpunthala3@gmail.com>
2025-01-03 11:23:28 +0530

    dsa

--- flowchart and pseudocode/pseudocode.py
+++ flowchart and pseudocode/pseudocode.py
@@ -12,4 +12,18 @@ Begin
     print sum
 end
 
+Flowchart 
+1. Flowchart is a graphical representation of a processs,system or algorithm
+2. It uses symbols and arrows to show the sequence of steps needed to complete a task
+3. Flow chart are useful in visualising how to solve a problem and make the process easier to follow
+
+Process to solve a problem (input,output,constraint)
+1. Analyze your problem
+2. Break down problem into smaller subports
+3. Remember/enlist the concept.
+4. Take 2-3 examples. (clear out confusion/gap)
+5. write a pseudocode on a paper.
+6. Dry run it 
+7. write down your solution
+8. Look out for edge cases \ boundary condition.** 
 '''
